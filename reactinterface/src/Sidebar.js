6c6dfefa684be34766d87bdf7fbd254ff05d1b9f
Collin Katz <collin.katz@gmail.com>
2024-02-28 18:03:41 -0500

    Added Rudimentary logo, Sidebar in style of other loyola site, and navbar

---
+++ reactinterface/src/Sidebar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Box, Button, Flex, Text, VStack } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { Link as ChakraLink, LinkProps } from '@chakra-ui/react'
+import { useNavigate, useLocation } from 'react-router-dom';
+
+function Sidebar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    function handleNavigate(path) {
+        return () => navigate(path);
+    }
+    
+    function whichButtonVariant(route) {
+        if (location.pathname === route) {
+            return "activeSidebar"
+        } else {
+            return "sidebar"
+        }
+    }
+
+    return (
+        <div className='Sidebar hide-for-small-only'>
+            <Flex
+                h="100vh"
+                w="20vw"
+            >
+                <Box // Sidebar background
+                    as="aside"
+                    w={{ base: "100%" }}
+                    bg="brand.darkGrey"
+                    // p="3px"
+                >
+                    <VStack align="stretch" spacing="0px">
+                        {/* Sidebar content here */}
+                        <Button onClick={handleNavigate("/")} variant={whichButtonVariant("/")}>Dashboard</Button>
+                        <Button onClick={handleNavigate("/classes")} variant={whichButtonVariant("/classes")}>Classes</Button>
+                        <Button onClick={handleNavigate("/intramural")} variant={whichButtonVariant("/intramural")}>Intramural</Button>
+                        <Button onClick={handleNavigate("/clubsports")} variant={whichButtonVariant("/clubsports")}>Club Sports</Button>
+                        <Button onClick={handleNavigate("/forms")} variant={whichButtonVariant("/forms")}>Forms</Button>
+                        <Button onClick={handleNavigate("/schedule")} variant={whichButtonVariant("/schedule")}>Schedule</Button>
+                        {/* Add more Links as needed */}
+                    </VStack>
+                </Box>
+            </Flex>
+        </div>
+    );
+}
+
+export default Sidebar;
