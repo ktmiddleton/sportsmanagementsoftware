c3c30011113549c9886cc51ee98bbd60f9e6ba98
collin k <collin.katz@gmail.com>
2024-03-24 23:08:15 -0400

    added base user class and register endpoints at /user/register

--- sportsserver/sportsserver/urls.py
+++ sportsserver/sportsserver/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("classes/", include("classes.urls")),
     path("admin/", admin.site.urls),
+    path("user/", include("user.urls"),)
 ]
--- sportsserver/user/views.py
+++ sportsserver/user/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from user.serializers import UserSerializer
+from django.contrib.auth.models import User
+from user.models import User as api_User
 
 # Create your views here.
+
+class UserRegister(APIView):
+    #Creates the user.
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token, created = Token.objects.get_or_create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
