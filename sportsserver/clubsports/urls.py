d3bf561a5946d2130ae2ad94d9d6f050612350c0
collin k <collin.katz@gmail.com>
2024-03-25 23:53:15 -0400

    added endpoints and methods to handle users joining teams and listing a users teams

--- sportsserver/clubsports/urls.py
+++ sportsserver/clubsports/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.ClubSportsTeamsList.as_view(), name="index"),
+    path("userteams/", views.UserTeamsList.as_view(), name="userteams"),
+    path("userjointeam/", views.UserJoinTeam.as_view(), name="userjointeam"),
 ]
--- sportsserver/clubsports/views.py
+++ sportsserver/clubsports/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from clubsports.serializers import ClubSportsTeamSerializer
 from clubsports.models import ClubSportsTeam
+from user.models import User
 
 # Create your views here.
 
@@ -19,3 +21,45 @@ class ClubSportsTeamsList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserTeamsList(APIView):
+
+    """
+    data format:
+    {
+        "username": ""
+    }
+    """
+    def get(self, request):
+        username = request.GET.get("username","default_value")
+        print(request.data)
+        user = User.objects.get(username=username)
+        if user is not None:
+            teams = ClubSportsTeam.objects.filter(members=user.pk)
+            serializer = ClubSportsTeamSerializer(teams, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+class UserJoinTeam(APIView):
+
+    """
+    data format:
+    {
+        "token": "",
+        "teamId": #
+    }
+    """
+    def post(self, request):
+        team_id = request.data.get("teamId")
+        token = request.data.get("token")
+        team = ClubSportsTeam.objects.get(id=team_id)
+        user = Token.objects.get(key=token).user
+        if user is not None and team is not None:
+            if team.registration == "open": # TODO: Make sure this is able to handle if a user has an invite
+                team.members.add(user.pk)
+                return Response(status=status.HTTP_201_CREATED)
+            else:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
