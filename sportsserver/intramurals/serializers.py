27a50770b17be79a300ae5cc78a40e2284a93c0d
Collin Katz <collin.katz@gmail.com>
2024-03-29 18:35:10 -0400

    added an intramuralsportsteamserializer and added a memberserializer to shorten json

--- sportsserver/intramurals/serializers.py
+++ sportsserver/intramurals/serializers.py
@@ -1,7 +1,21 @@
-from intramurals.models import IntramuralSport
+from intramurals.models import IntramuralSport, IntramuralSportTeam
 from rest_framework import serializers
+from user.models import User
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name')
 
 class IntramuralSportSerializer(serializers.ModelSerializer):
     class Meta:
         model = IntramuralSport
+        fields = '__all__'
+        
+class IntramuralSportTeamSerializer(serializers.ModelSerializer):
+    
+    members = MemberSerializer(many=True, required=False)
+    
+    class Meta:
+        model = IntramuralSportTeam
         fields = '__all__'
