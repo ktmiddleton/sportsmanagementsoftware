70fc3b22d8e30a89b4858dc70e220b4195eb413e
Collin Katz <collin.katz@gmail.com>
2024-03-29 18:32:20 -0400

    removed userjointeam/ url it was unnecessary

--- sportsserver/clubsports/urls.py
+++ sportsserver/clubsports/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path("", views.ClubSportsTeamsList.as_view(), name="index"),
     path("userteams/", views.UserTeamsList.as_view(), name="userteams"),
-    path("userjointeam/", views.UserJoinTeam.as_view(), name="userjointeam"),
 ]
