2d9b6de5f64094575758d550c9e0df49bf8efda1
Collin Katz <collin.katz@gmail.com>
2024-03-29 18:35:57 -0400

    added urls to list intramural teams, and to list user teams/handle user joining teams

--- sportsserver/intramurals/urls.py
+++ sportsserver/intramurals/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.IntramuralSportList.as_view(), name="index"),
+    path("teams/", views.IntramuralSportTeamList.as_view(), name="teams"),
+    path("userteams/", views.UserTeamsList.as_view(), name="userteams"),
 ]
