3ad817ccfe51661bdbf957bbe8d6f2c5730f8701
Collin Katz <collin.katz@gmail.com>
2024-04-09 16:12:04 -0400

    Changed buttons

--- reactinterface/src/components/IntramuralCard.js
+++ reactinterface/src/components/IntramuralCard.js
@@ -5,6 +5,8 @@ import { useUser } from "./UserContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+const REGISTRATION_DEADLINE_PASSED_TEXT = "Completed";
+
 export default function IntramuralCard({sportData, image}) {
 
     const [joinSubmitting, setJoinSubmitting] = useState(false);
@@ -32,7 +34,7 @@ export default function IntramuralCard({sportData, image}) {
         let timeString = "";
 
         if (diffMilli < 0) {
-            timeString = "Completed"
+            timeString = REGISTRATION_DEADLINE_PASSED_TEXT;
         } else if (daysRemaining >= 1) {
             timeString = daysRemaining + " days, " + hoursRemaining + " hours"
         } else if (hoursRemaining >= 1) {
@@ -69,32 +71,32 @@ export default function IntramuralCard({sportData, image}) {
     }
 
     function register(event) {
-        event.stopPropagation(); // Prevent the outter button from being clicked that navigates to other page
-        axios.post(`http://localhost:8000/classes/userclasses/`,
-            {
-                token: localStorage.getItem("token"),
-                classId: sportData.id
-            }
-        )
-        .then((response) => {
-            window.location.reload();
-            toast({
-                title: 'Class Joined.',
-                description: "You've successfully joined class " + sportData.name + ".",
-                status: 'success',
-                duration: 9000,
-                isClosable: true,
-            })
-        })
-        .catch((error) => {
-            toast({
-                title: 'Failed to Join Class.',
-                description: "You've encountered an error joining Class " + sportData.name + ".",
-                status: 'error',
-                duration: 9000,
-                isClosable: true,
-            })
-        })
+        // event.stopPropagation(); // Prevent the outter button from being clicked that navigates to other page
+        // axios.post(`http://localhost:8000/classes/userclasses/`,
+        //     {
+        //         token: localStorage.getItem("token"),
+        //         classId: sportData.id
+        //     }
+        // )
+        // .then((response) => {
+        //     window.location.reload();
+        //     toast({
+        //         title: 'Class Joined.',
+        //         description: "You've successfully joined class " + sportData.name + ".",
+        //         status: 'success',
+        //         duration: 9000,
+        //         isClosable: true,
+        //     })
+        // })
+        // .catch((error) => {
+        //     toast({
+        //         title: 'Failed to Join Class.',
+        //         description: "You've encountered an error joining Class " + sportData.name + ".",
+        //         status: 'error',
+        //         duration: 9000,
+        //         isClosable: true,
+        //     })
+        // })
         setJoinSubmitting(true);
         setTimeout(() => {
             setJoinSubmitting(false);
@@ -104,10 +106,10 @@ export default function IntramuralCard({sportData, image}) {
     function getButtons() {
         var element = "";
         let action = "" // TODO: Need to also check if user has permissions to register
-        if (sportData.registered_participants < sportData.capacity) {
-            action = "open"
+        if (calculateTimeRemaining(sportData.registration_deadline) !== REGISTRATION_DEADLINE_PASSED_TEXT ) {
+            action = "goToRegistration";
         } else {
-            action = "full"
+            action = "registerClosed";
         }
 
         // if (sportData.members.some((member) => member.username === localStorage.getItem("username"))) {
@@ -116,8 +118,8 @@ export default function IntramuralCard({sportData, image}) {
 
         switch (action) 
         {
-            case "open":
-                element = <Button isLoading={joinSubmitting} onClick={(event) => register(event)} m="2" variant="Register">Register</Button>;
+            case "goToRegistration":
+                element = <Button isLoading={joinSubmitting} onClick={(event) => register(event)} m="2" variant="Register">Registration</Button>;
                 break;
             case "waitlist":
                 element = <Button m="2" variant="Waitlist">Waitlist</Button>;
@@ -131,8 +133,8 @@ export default function IntramuralCard({sportData, image}) {
             case "registerOpen":
                 element = <Flex><Button m="2" variant="Register">Competitive</Button> <Button m="2" variant="Register">Casual</Button></Flex>;
                 break;
-            case "registerClose":
-                element = <Flex><Button isDisabled="true" m="2" variant="Full">Closed</Button> <Button isDisabled="true" m="2" variant="Full">Closed</Button></Flex>
+            case "registerClosed":
+                element = <Flex><Button isDisabled="true" m="2" variant="Full">Closed</Button> {/*<Button isDisabled="true" m="2" variant="Full">Closed</Button>*/}</Flex>
                 break;
             case "registerRequest":
                 element = <Flex><Button m="2" variant="Waitlist">By Request</Button> <Button m="2" variant="Waitlist">By Request</Button></Flex>
