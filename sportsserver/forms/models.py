112ab33c4147c753f907b6b011b1ce959794e1fc
collin k <collin.katz@gmail.com>
2024-04-11 01:52:52 -0400

    Removed unnecessary comments

--- sportsserver/forms/models.py
+++ sportsserver/forms/models.py
@@ -9,10 +9,9 @@ class FormInfo(models.Model):
     name=models.CharField(max_length=50)
     body=models.TextField(null=True)
 
-    # Triggers that define when a new form gets added to a user
+    # TODO: Triggers that define when a new form gets added to a user
     clubsport_join=models.BooleanField(blank=True, null=False, default=False)
     class_join=models.BooleanField(blank=True, null=False, default=False)
-    # intramural_team_join=models.BooleanField(blank=True, default=False)
     intramural_team_join=models.BooleanField(blank=True, null=False, default=False)
 
 # Create your models here. 
@@ -23,17 +22,4 @@ class Form(models.Model):
     user=models.ForeignKey(User, blank=False, null=True, on_delete=models.CASCADE)
     clubsport=models.ForeignKey(ClubSportsTeam, blank=True, null=True, on_delete=models.CASCADE)
     class_obj=models.ForeignKey(Class, blank=True, null=True, on_delete=models.CASCADE)
-    # intramural_sport=models.ForeignKey(IntramuralSport, blank=True, null=True, on_delete=models.CASCADE)
     intramural_team=models.ForeignKey(IntramuralSportTeam, blank=True, null=True, on_delete=models.CASCADE)
-
-# class ClubSportForm(Form):
-#     clubsport=models.ForeignKey(ClubSportsTeam, blank=True)
-
-# class ClassForm(Form):
-#     class_=models.ForeignKey(Class, blank=True)
-
-# class IntramuralSportForm(Form):
-#     intramural_sport=models.ForeignKey(IntramuralSport, blank=True)
-
-# class IntramuralTeamForm(Form):
-#     intramural_team=models.ForeignKey(IntramuralSportTeam, blank=True)
